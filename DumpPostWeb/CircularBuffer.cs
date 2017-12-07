a89df7f1dc697efdcaf4d17bc6a7eb5f33a63273
James World <james-world@users.noreply.github.com>
2017-12-07 11:41:35 +0000

    Refactor, and add datestamp and path

---
+++ DumpPostWeb/CircularBuffer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DumpPostWeb
+{
+    public class CircularBuffer : IEnumerable<string>
+    {
+        private object gate = new object();
+        private List<string> items;
+        public int Length { get; }
+        
+
+        public CircularBuffer(int length)
+        {
+            Length = length;
+            items = new List<string>(length);
+        }
+
+        public void Add(string item)
+        {
+            lock (gate)
+            {
+                items.Add(item);
+                if (items.Count > Length)
+                {
+                    items.RemoveAt(0);
+                }
+            }
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            lock (gate)
+            {
+                return items.ToList().AsReadOnly().AsEnumerable().GetEnumerator();
+            }
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- DumpPostWeb/Startup.cs
+++ DumpPostWeb/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +14,8 @@ namespace DumpPostWeb
 {
     public class Startup
     {
+        private static readonly CircularBuffer Posts = new CircularBuffer(3);
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,7 +32,44 @@ namespace DumpPostWeb
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Method == "GET" && context.Request.Path == "/show" )
+                {
+                    var response = new StringBuilder();
+                    response.AppendLine("<html><body><ul>");
+                    foreach (var post in Posts.Reverse())
+                    {
+                        response.AppendLine($"<li>{post}</li>");
+                    }
+                    response.AppendLine("</ul></body></html>");
+                    await context.Response.WriteAsync(response.ToString());
+                }
+                else if (context.Request.Method == "GET" && context.Request.Path == "/favicon.ico")
+                {
+                    context.Response.StatusCode = 200;
+                    await context.Response.SendFileAsync("favicon.ico");
+                }
+                else
+                {
+                    var content = new StringBuilder();
+                    content.AppendFormat("DateTime:{0}<br>", DateTime.UtcNow);
+                    content.AppendFormat("Path:{0}<br>", context.Request.Path);
+                    foreach (var header in context.Request.Headers)
+                    {
+                        content.AppendFormat("{0}:{1}<br>", header.Key, header.Value);
+                    }
+
+                    var stream = new StreamReader(context.Request.Body);
+
+                    content.AppendLine("Body:<br>");
+                    var body = await stream.ReadToEndAsync();
+                    var encodedBody = HttpUtility.HtmlEncode(body);
+                    content.AppendLine(encodedBody);
+
+                    Posts.Add(content.ToString());
+
+                    context.Response.StatusCode = 200;
+                    await context.Response.WriteAsync("Thanks");
+                }
             });
         }
     }
